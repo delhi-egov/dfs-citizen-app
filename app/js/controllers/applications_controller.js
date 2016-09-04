7a8a888641bbc8c22df54e320b70736268de9cef
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-09-04 20:41:23 +0530

    Added new assets and fixed some issues

--- app/js/controllers/applications_controller.js
+++ app/js/controllers/applications_controller.js
@@ -3,6 +3,14 @@ module.exports = function(dashboardService, userService) {
     this.resume= function(application) {
         dashboardService.resumeApplication(this, application);
     };
+    this.takeAction = function(application) {
+        if(application.stage === 'WAITING_ON_USER') {
+            //Add edit action here
+        }
+        else if(application.stage !== 'COMPLETE') {
+            this.resume(application);
+        }
+    };
     userService.me(this).then(function() {
     	dashboardService.getApplications(that);
     });
